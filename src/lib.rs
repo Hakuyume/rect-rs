6ea44c562f9eda912f07fa9adbaafc68af0e5a0b
Toru Ogawa <Hakuyume@users.noreply.github.com>
2019-02-09 14:40:04 +0900

    add by_key

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 mod nms;
 mod rect;
 
-pub use self::nms::non_maximum_suppression;
+pub use self::nms::{non_maximum_suppression, non_maximum_suppression_by_key};
 pub use self::rect::Rect;
--- src/nms.rs
+++ src/nms.rs
@@ -14,9 +14,22 @@ where
     });
 }
 
-fn retain_with_prevs<T, F>(v: &mut Vec<T>, f: F)
+pub fn non_maximum_suppression_by_key<I, F, R, T>(items: &mut Vec<I>, mut f: F, thresh: T)
 where
-    F: Fn(&T, &[T]) -> bool,
+    F: FnMut(&I) -> R,
+    R: Rect<T>,
+    T: Clone + PartialOrd + Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T>,
+{
+    retain_with_prevs(items, |i, prevs| {
+        prevs
+            .iter()
+            .all(|p| f(i).iou(&f(p)).map_or(true, |iou| iou < thresh))
+    });
+}
+
+fn retain_with_prevs<T, F>(v: &mut Vec<T>, mut f: F)
+where
+    F: FnMut(&T, &[T]) -> bool,
 {
     let len = (0..v.len()).fold(0, |l, r| {
         v.swap(l, r);
